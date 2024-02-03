012b4b6a07cd31c829780a639e2303a381b8c69c
brunoziie <eu@brunoziie.com>
2024-02-02 21:29:57 -0300

    feat: add tsparticle

---
+++ frontend/src/components/ui/particle-background.tsx
@@ -0,0 +1,66 @@
+'use client';
+import Particles, { initParticlesEngine } from '@tsparticles/react';
+import { loadSlim } from '@tsparticles/slim';
+import { useEffect, useMemo, useState } from 'react';
+
+export default function ParticleBackground() {
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const options = useMemo(
+    () => ({
+      background: { color: { value: '#fff' } },
+      fpsLimit: 120,
+      interactivity: {
+        modes: {
+          push: {
+            quantity: 4,
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4,
+          },
+        },
+      },
+      particles: {
+        color: { value: '#CCC' },
+        links: {
+          color: '#CCC',
+          distance: 150,
+          enable: true,
+          opacity: 0.5,
+          width: 1,
+        },
+        move: {
+          direction: 'none',
+          enable: true,
+          outModes: {
+            default: 'bounce',
+          },
+          random: false,
+          speed: 1,
+          straight: false,
+        },
+        number: { density: { enable: true }, value: 120 },
+        opacity: { value: 0.5 },
+        shape: { type: 'circle' },
+        size: { value: { min: 1, max: 2 } },
+      },
+      detectRetina: true,
+    }),
+    []
+  );
+
+  if (init) {
+    return <Particles id="tsparticles" options={options as any} />;
+  }
+
+  return <></>;
+}
